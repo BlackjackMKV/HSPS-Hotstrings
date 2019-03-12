b0c9db30e359ea8e6e75c60d4176acc411c090c8
BlackjackMKV <techniclight@yahoo.com>
2019-03-12 09:07:29 -0600

    Initial Installer Implementation
    Added basic code to get the installer functional. Will come back to refine it later.

---
+++ installer.bat
@@ -0,0 +1,9 @@
+::@echo off
+
+copy "Essentials-19.03.06.01.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "BasicInstalls-19.03.06.01.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "AdvancedInstalls-19.03.06.01.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "DTX-EZD Core-19.03.06.01.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "eTrans-19.03.06.01.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+pause
